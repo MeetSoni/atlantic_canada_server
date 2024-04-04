f155930fc6d953f2c9405b9c9103e1af05b81a06
Meet Soni <64322534+MeetSoni@users.noreply.github.com>
2024-04-04 09:03:17 -0400

    sending more data in login

--- controller/authcontroller.js
+++ controller/authcontroller.js
@@ -89,7 +89,7 @@ async function handleUserLogin(req, res) {
 
     res.send({
       token:token,
-      email:user.email
+      data:{email:user.email, province_id:user.province_id }
     })
 
   } catch (error) {
